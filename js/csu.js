216c80032f1c71ceda76cde42c3cff517abaa10c
Sem620 <Solveig.Mitchell@gmail.com>
2017-05-09 14:00:22 -0700

    Add files via upload

---
+++ js/csu.js
@@ -0,0 +1,352 @@
+var csu = 
+	{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Humboldt",
+        "Retention": 74,
+        "Population": 7983
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -124.0778,
+          40.8753
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Chico",
+        "Retention": 87,
+        "Population": 16081
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.8473,
+          39.7296
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Sonoma",
+        "Retention": 81,
+        "Population": 8512
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6742,
+          38.3394
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Maritime",
+        "Retention": 83,
+        "Population": 1047
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.2306,
+          38.0697
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "San Francisco",
+        "Retention": 84,
+        "Population": 28394
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.4782,
+          37.7219
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "East Bay",
+        "Retention": 81,
+        "Population": 12340
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.0564,
+          37.6577
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Sacramento",
+        "Retention": 83,
+        "Population": 26739
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.4241,
+          38.5604
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Stanislaus",
+        "Retention": 85,
+        "Population": 7847
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -120.8569,
+          37.5246
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "San Jose",
+        "Retention": 87,
+        "Population": 26664
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.8811,
+          37.3352
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Monterey Bay",
+        "Retention": 83,
+        "Population": 6231
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.8017,
+          36.6538
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Fresno",
+        "Retention": 83,
+        "Population": 20511
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.7461,
+          36.8134
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "San Luis Obisbo",
+        "Retention": 93,
+        "Population": 19246
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -120.6625,
+          35.305
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Bakersfield",
+        "Retention": 73,
+        "Population": "7,635"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.1033,
+          35.3486
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Northridge",
+        "Retention": 77,
+        "Population": 35616
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -118.5281,
+          34.2426
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Ponoma",
+        "Retention": 89,
+        "Population": 22395
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -117.7118,
+          34.0977
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Channel Islands",
+        "Retention": 76,
+        "Population": 5677
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.0434,
+          34.1621
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Los Angeles",
+        "Retention": 84,
+        "Population": 20681
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -118.1681,
+          34.0668
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "San Bernadino",
+        "Retention": 88,
+        "Population": 16743
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -117.3235,
+          34.1822
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Dominguez Hills",
+        "Retention": 80,
+        "Population": 12682
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -118.2553,
+          33.8634
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Fullerton",
+        "Retention": 89,
+        "Population": 32726
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -117.8869,
+          33.8829
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "San Marcos",
+        "Retention": 83,
+        "Population": 11559
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -117.1599,
+          33.1296
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "Long Beach",
+        "Retention": 90,
+        "Population": 31643
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -118.1141,
+          33.7838
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "School": "San Diego",
+        "Retention": 88,
+        "Population": 28394
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -117.0719,
+          32.7757
+        ]
+      }
+    }
+  ]
+};
+// JavaScript Document
